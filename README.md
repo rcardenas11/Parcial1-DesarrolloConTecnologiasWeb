b2cb7e84d671279c99387cc0ea306380cb4de483
rafael <r.cardenas11@uniandes.edu.co>
2018-09-20 09:18:30 -0500

    parcial
    parcial

---
+++ README.md
@@ -0,0 +1,57 @@
+# Parcial 1
+
+Este proyecto fue realizado por Rafael Cárdenas Gasca (rcardenas11) para el parcial 1 de la clase de programación con tecnologías web de la Universidad de los Andes.
+
+# Características
+
+
+Servidor Público:
+- Heroku
+
+Back - End:
+- MongoDB
+- Nodejs
+- Expressjs
+- JavaScript
+
+Front-End:
+- React
+- CSS
+- Bootstrap
+- JavaScript
+
+# Prerrequisitos
+- Instalar NodeJs
+# Despliegue
+1. Clonar (o descargar) el repositorio .
+2. Navegue por consola hasta el directorio del proyecto y corra el comando
+```
+> npm install
+```
+3. Navegue por consola hasta el directorio /front y corra nuevamente el comando
+```
+> npm install
+```
+4. Verifique que exista una carpeta llamada **node_modules** tanto en el directorio raíz  del proyecto como en el directorio  /front .
+5. Vuelva nuevamente al directorio /front y ejecute el comando
+```
+> npm start
+```
+6. Finalmente, en el directorio raíz ejecute el comando 
+```
+> npm start
+```
+
+Encontrará la aplicación desplegada si en su browser se dirige a http://localhost:3000/
+
+
+# ScreenShot
+
+Página del proyecto:
+# Licencia MIT
+
+El proyecto cuenta con la licencia del MIT disponible [aquí](https://github.com/daramirezv/servicios-uniandes/blob/master/LICENSE).
+
+# Autor
+
+- [Rafael Cárdenas](https://rcardenas11.github.io/)
---
+++ routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+function getDatos(callback){
+
+  // Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';  //poner el nombre de la base de datos
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  findDocuments(db,(data) => {
+    callback(data);
+    client.close();
+  } );
+ 
+  
+});
+}
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');  // nombre de la coleccion
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+   
+    callback(docs);
+  });
+}
+
+const insertDocuments = function (req, string, db, callback) {
+  // Get the documents collection
+  const collection = db.collection(string);
+  // Insert some documents
+  const objeto = req.query;
+
+  collection.insertMany([objeto],
+    function (err, result) {
+      assert.equal(err, null);
+      callback(result);
+    });
+}
+
+
+
+
+
+/* GET home page. */
+router.get('/getData', function(req, res, next) {
+  getDatos((data) =>
+    res.send(data) 
+    );
+
+});
+
+module.exports = router;
